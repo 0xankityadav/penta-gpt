67a8171c9b69347aad4ec51b6038d1b9d02965ea
Ankit Yadav <ankityadav3152@gmail.com>
2023-07-23 18:49:41 +0530

    change landing page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,16 +1,13 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { LandingNavbar } from "@/components/landing-navbar";
+import { LandingHero } from "@/components/landing-hero";
+import { LandingContent } from "@/components/landing-content";
 
 const LandingPage = () => {
   return (
-    <div>
-      <div>Landing Page (Unprotected)</div>
-      <Link href="/sign-in">
-        <Button>Login</Button>
-      </Link>
-      <Link href="/sign-up">
-        <Button>Register</Button>
-      </Link>
+    <div className="h-full ">
+      <LandingNavbar />
+      <LandingHero />
+      <LandingContent />
     </div>
   );
 };
